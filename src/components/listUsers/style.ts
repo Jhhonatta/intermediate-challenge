f20f7e6de782dbd6f13591f905cad250964fdff2
Jhhonatta <jhonatta_martins@hotmail.com>
2023-09-14 12:46:11 -0300

    add reset.css

---
+++ src/components/listUsers/style.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ContainerListUser = styled.div`
+  display: flex;
+  margin-top: 10px;
+
+  ul {
+    width: max-content;
+  }
+
+  .columnLi {
+    color: #ffffff;
+    font-family: Poppins;
+    background-color: #811ac0;
+    width: 100px;
+    margin-right: 3px;
+  }
+
+  li {
+    color: #d6d6d6;
+    font-family: Poppins;
+    height: 50px;
+    width: 200px;
+  }
+`;
